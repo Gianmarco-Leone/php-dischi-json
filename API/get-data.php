8c8b563d50e8997fe7622a7d84e9261f7aae5b71
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-03-15 10:03:37 +0100

    fixes php + modify scaffolding

--- API/get-data.php
+++ API/get-data.php
@@ -4,11 +4,12 @@
 $data = file_get_contents(__DIR__ . "/../data/cds.json");
 
 // Lo strasformo in un array leggibile per PHP (tramite metodo decode)
-$data_decoded = json_decode($data);
+// $data_decoded = json_decode($data);
 
 // Intesto risposta per avvisare che il contenuto è un JSON
 header("Content-Type: application/json; charset=utf-8");
 
 // Stampo l'array trasformato (tramite metodo encode) in un contenuto JSON
-echo json_encode($data_decoded);
+// echo json_encode($data_decoded);
+echo $data;
 ?>
